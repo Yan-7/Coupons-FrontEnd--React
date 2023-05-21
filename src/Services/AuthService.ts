80d719439adf5aa055152b5ecb576e3d85c2b884
bob <yanivalmagor7@gmail.com>
2023-05-21 13:28:22 +0300

    21/5b

--- src/Services/AuthService.ts
+++ src/Services/AuthService.ts
@@ -1,7 +1,6 @@
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 import UserModel from "../Models/UsreModel";
 import appConfig from "../Utils/Config";
-// import { url } from "inspector";
 import { authStore, loginAction, logoutAction, registerAction } from "../Redux/AuthenticationState";
 import CredentailsModel from "../Models/CredentailsModel";
 
@@ -11,23 +10,32 @@ class AuthService {
       const response = await axios.post<string>(appConfig.registerUrl, user); // Added 'user' to the request
       const token = response.data;
       authStore.dispatch(registerAction(token));
-    } catch (error) {
+    } catch (err) {
+      const error = err as AxiosError; // Type assertion here
       console.error("Failed to register user", error);
       throw error; // Throwing error for higher level error handling
     }
   }
 
-async login(credentials: CredentailsModel): Promise<void> {
-  const response = await axios.post<string>(appConfig.loginUrl, credentials);
-  const token = response.data;
-  authStore.dispatch(loginAction(token));
-}
+  async login(credentials: CredentailsModel): Promise<void> {
+    try {
+      const response = await axios.post<string>(appConfig.loginUrl, credentials);
+      const token = response.data;
+      authStore.dispatch(loginAction(token));
+    } catch (err) {
+      const error = err as AxiosError; // Type assertion here
+      console.error("Failed to login", error);
+      if (error.response && error.response.status === 401) { 
+        throw new Error("Invalid username or password");
+      } else {
+        throw error; // Throwing error for higher level error handling
+      }
+    }
+  }
 
-public logout(): void {
-  authStore.dispatch(logoutAction())
-}
-
-
+  public logout(): void {
+    authStore.dispatch(logoutAction())
+  }
 }
 
 const authService = new AuthService();
